ff65c20a559efcd3e7be7e2558c9318571aa92e4
Jay Sharma <8313sharma.jay.sy@gmail.com>
2024-06-08 20:12:50 +0530

    updated

--- src/main/java/com/projects/sunrise_hotel/security/CorsConfig.java
+++ src/main/java/com/projects/sunrise_hotel/security/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173/");
+        config.addAllowedOrigin("https://sunrise-hotel-theta.vercel.app/");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
